ff142b7d23493ac4be32009d2edbcc2f5a21bb59
Kristoffer Karlsson <kristoffer.karlsson@solidar.se>
2018-01-15 13:01:22 +0100

    Minor fix in getContacts route

--- server/app/controllers/contacts/getContactsForUserController.js
+++ server/app/controllers/contacts/getContactsForUserController.js
@@ -6,15 +6,12 @@ const controllerHelper = require('../../helpers/controllerHelper');
 const invoke = async (data) => {
   const userGuid = data.userGuid;
   const contactGuids = await contactGetter.getContactsByGuid(userGuid);
-  
-  const users = await getFullyQualifiedContacts(contactGuids);
-  return controllerHelper.successResponse(200, users);
+  const contacts = await getFullyQualifiedContacts(contactGuids);
+  return controllerHelper.successResponse(200, contacts);
 }
 
-const getFullyQualifiedContacts = async (contactGuids) => {
-  const calls = Array.prototype.map.call(contactGuids, (guid) => userGetter.getUserByGuid(guid) );
-  const users = Promise.all(calls);
-  return users;
+const getFullyQualifiedContacts = (contactGuids) => {
+  return Promise.all(contactGuids.map(guid => userGetter.getUserByGuid(guid)));
 }
 
 module.exports = {invoke};
