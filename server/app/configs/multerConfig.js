540611033f71d545407d9a9a7d06ace636fa124b
kristofferkarlsson93 <kristoffer.karlsson93@hotmail.com>
2018-01-16 23:07:59 +0100

    started to configure Multer

--- server/app/configs/multerConfig.js
+++ server/app/configs/multerConfig.js
@@ -1,1 +1,23 @@
 const multer = require('multer');
+
+const fileFilter = (request, file, callback) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    callback(null, true); //all good
+  } else {
+    callback(null, false); // Not good
+  }
+};
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../../public/images');
+  },
+  filename: function (req, file, cb) {
+    const splitedName = file.originalname.split('.');
+    cb(null, file.filename + splitedName[splitedName.length-1]);
+  }
+});
+
+
+
