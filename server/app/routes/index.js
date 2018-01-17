0a2abb3d3c9bedf61b67c00b47e4f378d80b8ec0
Kristoffer Karlsson <kristoffer.karlsson@solidar.se>
2018-01-17 13:17:05 +0100

    More multer config, untested

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,8 +1,11 @@
 'use strict';
 const userRoutes = require('./userRoutes');
 const contactRoutes = require('./contactRoutes');
+const memoryRoutes = require('./memoryRoutes');
 
 module.exports = (app) => {
     userRoutes(app);
     contactRoutes(app);
+    memoryRoutes(app) //should this recieve the multer config instead?
+    
 }
