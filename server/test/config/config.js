4aafd60aeea2486675f3c3724ff7103d054af37b
kristofferkarlsson93 <kristoffer.karlsson93@hotmail.com>
2018-01-29 22:39:11 +0100

    Started to look at mocha to make REST-API tests

---
+++ server/test/config/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  baseUrl: 'localhost:8000',
+  should: require('chai').should,
+  expect: require('chai').expect,
+  fetch: require('node-fetch'),
+}
---
+++ server/test/userRoutes/userTests.js
@@ -0,0 +1,7 @@
+const testConfig = require('../config/config');
+
+describe('User', () => {
+  it('should be possible to add when not existing', () => {
+    console.log('hejsan din fis');
+  });
+});
